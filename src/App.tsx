0df147236d524ab3337fe620d7f64da76c20aae5
unknown <adebayooluwasegun335@gmail.com>
2024-10-07 08:53:38 +0100

    phew

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Destination from "./components/Destination";
 import Technology from "./components/Technology";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { DestinationData } from "./Types/DestinationData";
 import { CrewMember } from "./Types/CrewMember";
@@ -19,32 +19,30 @@ function App() {
   const [destination, setDestination] = useState<DestinationData[]>([]);
   const [crew, setCrew] = useState<CrewMember[]>([]);
   const [technology, setTechnology] = useState<TechnologyData[]>([]);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [destinationsResponse, crewResponse, technologyResponse] =
-          (await Promise.all([
-            axios.get("/data.json/destinations"),
-            axios.get("/data.json/crew"),
-            axios.get("/data.json"),
-          ])) as [
-            AxiosResponse<DestinationData[]>,
-            AxiosResponse<CrewMember[]>,
-            AxiosResponse<TechnologyData[]>
-          ];
+        const response = await axios.get("Data/data.json");
+        const data = response.data;
+        console.log(data);
+        // Assuming your `data.json` file has a `destinations` key
+        const destinations = data.destinations;
+        console.log(destinations);
+        const crew = data.crew;
+        const technology = data.technology;
 
-        setDestination(destinationsResponse.data);
-        console.log(destinationsResponse.data);
-        setCrew(crewResponse.data);
-
-        setTechnology(technologyResponse.data);
+        setDestination(destinations);
+        setCrew(crew);
+        setTechnology(technology);
       } catch (error) {
         console.error(error);
       }
     };
 
     fetchData();
-  }, []);
+  });
+
   return (
     <>
       <div className="app">
@@ -71,3 +69,28 @@ function App() {
 }
 
 export default App;
+// useEffect(() => {
+//   const fetchData = async () => {
+//     try {
+//       const [destinationsResponse, crewResponse, technologyResponse] =
+//         (await Promise.all([
+//           axios.get("/data.json"),
+//           axios.get("/data.json"),
+//           axios.get("/data.json"),
+//         ])) as [
+//           AxiosResponse<DestinationData[]>,
+//           AxiosResponse<CrewMember[]>,
+//           AxiosResponse<TechnologyData[]>
+//         ];
+//       setDestination(destinationsResponse.data);
+//       console.log(destinationsResponse.data);
+//       setCrew(crewResponse.data);
+
+//       setTechnology(technologyResponse.data);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   fetchData();
+// }, []);
